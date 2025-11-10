c8bfa180e2b90380b753e6de85a85f804b11d8d3
Denys Koval <metalheadden@gmail.com>
2025-11-10 15:11:43 +0200

    hero,join,about

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,5 +1,26 @@
+import Link from "next/link";
 import css from "./Hero.module.css";
 
 export default function Hero() {
-  return <section>hero</section>;
+  return (
+    <section className={css.hero}>
+      <div className="container">
+        <div className={css.contentbox}>
+          <h1 className={css.title}>Відкрийте світ подорожей з нами!</h1>
+          <p className={css.text}>
+            Приєднуйтесь до нашої спільноти мандрівників, де ви зможете ділитися
+            своїми історіями та отримувати натхнення для нових пригод. Відкрийте
+            для себе нові місця та знайдіть однодумців!
+          </p>
+          <Link
+            className={css.button}
+            href="/auth/register"
+            aria-label="Register"
+          >
+            Доєднатись
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
 }

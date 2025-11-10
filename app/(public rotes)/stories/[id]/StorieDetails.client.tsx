0f8a3a17e7ca0d7024e9582d2174c0707d39c20f
juliya.lysenko <juliya.deleh@gmail.com>
2025-11-10 11:55:06 +0200

    commit1

--- app/(public rotes)/stories/[id]/StorieDetails.client.tsx
+++ app/(public rotes)/stories/[id]/StorieDetails.client.tsx
@@ -3,7 +3,27 @@
 import PopularStories from "@/components/PopularStories/PopularStories";
 import css from "./StorieDetails.module.css";
 
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "next/navigation";
+import { fetchNoteById } from "@/lib/api/clientApi";
+
 const StorieDetailsClient = () => {
+  const { id } = useParams<{ id: string }>();
+
+  const {
+    data: note,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["note", id],
+    queryFn: () => fetchNoteById(id),
+    refetchOnMount: false,
+  });
+
+  if (isLoading) return <p>Loading...</p>;
+
+  if (error || !note) return <p>Some error..</p>;
+
   return (
     <div>
       StorieDetailsClient
--- app/(public rotes)/stories/[id]/page.tsx
+++ app/(public rotes)/stories/[id]/page.tsx
@@ -5,6 +5,7 @@ import {
   QueryClient,
 } from "@tanstack/react-query";
 import { Metadata } from "next";
+import { fetchServerNotebyId } from "@/lib/api/serverApi";
 
 interface Props {
   params: Promise<{ id: string }>;
@@ -31,7 +32,13 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
 }
 
 const StorieDetails = async ({ params }: Props) => {
+  const { id } = await params;
   const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ["note", id],
+    queryFn: () => fetchServerNotebyId(id),
+  });
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>

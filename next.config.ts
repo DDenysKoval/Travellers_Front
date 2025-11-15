6989b2df530abc9dfc5d37f3b15ca5c94f39d342
Denys Koval <metalheadden@gmail.com>
2025-11-15 19:59:36 +0200

    nextconfig

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    domains: ['res.cloudinary.com'],
     remotePatterns: [
       { protocol: 'https', hostname: 'ac.goit.global' },
       { protocol: 'https', hostname: 'ftp.goit.study' },
-		  // { protocol: 'https', hostname: 'isorepublic.com' }
     ]
 	}
 };

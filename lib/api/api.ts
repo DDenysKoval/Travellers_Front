809de4ca7c83f6ad8397a62db59cb344d20995ce
Denys Koval <metalheadden@gmail.com>
2025-11-15 20:11:33 +0200

    conflict1

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,11 +1,5 @@
 import axios from "axios";
 
-// const baseURL = process.env.NEXT_PUBLIC_API_URL;
-// export const nextServer = axios.create({
-//   baseURL: baseURL,
-//   withCredentials: true,
-// });
-
 const baseURL = process.env.NEXT_PUBLIC_API_URL + "/api";
 export const nextServer = axios.create({
   baseURL: baseURL,

8520c9806b59aa90ba65dd795370dd77d33b37c9
juliya.lysenko <juliya.deleh@gmail.com>
2025-11-16 09:41:15 +0200

    commit10

--- types/story.ts
+++ types/story.ts
@@ -3,8 +3,11 @@ export interface StoryId {
   img: string;
   title: string;
   article: string;
-  category: string;
-  ownerId?: string;
+
+  category: Category | null;
+
+  author: User | null;
+
   date: string;
   favoriteCount: number;
 }
@@ -13,4 +16,16 @@ export interface StoryWrapper {
   status: number;
   message: string;
   data: StoryId;
+}
+
+export interface User {
+  _id: string;
+  username: string;
+  email?: string;
+  avatar?: string;
+}
+
+export interface Category {
+  _id: string;
+  name: string;
 }
